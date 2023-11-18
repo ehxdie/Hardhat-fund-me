202a2b1b21ee245260a83e4b66215084f9f70333
Edidiong <ephraimedy@gmail.com>
2023-11-18 19:21:47 +0100

    Compiled the mock test contract

--- contracts/test/MockV3Aggregator.sol
+++ contracts/test/MockV3Aggregator.sol
@@ -1,4 +1,5 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "./@chainLink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
 
